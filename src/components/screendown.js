73c52e3b1676191702228dc7890a3c1cc1aedb64
Suelton Souza <sueltonlima@gmail.com>
2020-12-11 11:54:24 -0300

    style(style list and scroll): screen down style table list and scroll

--- src/components/screendown.js
+++ src/components/screendown.js
@@ -1,11 +1,73 @@
 import React from 'react';
-  
+import '../styles/screendown.css';
+
 function ScreenDown() {        
     return (
         <div class="format-screen-two">
             <div class="container-screen-two">
                 <div class="stick-control"></div>
-                <div class="screen-two"></div>
+                <div class="screen-two">
+                        <div class="scroll-table">
+                            <table class="table">
+                                <thead>
+                                    <th>Image</th>
+                                    <th>Pokemon</th>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td>Bubasauro</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>Bubasauro</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>Bubasauro</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>Bubasauro</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>Bubasauro</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>Bubasauro</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>Bubasauro</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>Bubasauro</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>Bubasauro</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>Bubasauro</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>Bubasauro</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>Bubasauro</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div class="space-final-scroll"></div>
+                        </div>
+                        
+                </div>
                 <div class="buttons">
                     <div class="bt"><span></span></div>
                     <div class="bt-row">
